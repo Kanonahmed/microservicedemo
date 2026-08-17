1fc2832614b6f8c6337518561971fc8da7638efd
kanon <kanoncse41@gmail.com>
2026-08-17 15:50:08 +0600

    CatalogDbContext created

---
+++ src/Services/Catalog.API/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Models
+{
+    public class Products
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Summery { get; set; }
+        public string ImageFile { get; set; }
+        public Decimal Price { get; set; }
+
+    }
+}
