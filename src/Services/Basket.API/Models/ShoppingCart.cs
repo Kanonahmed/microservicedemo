129493df0540e824175d804a4b37678249ddcba8
kanon <kanoncse41@gmail.com>
2026-09-01 12:21:04 +0600

    Connection built between redis and app

---
+++ src/Services/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        ShoppingCart(string username)
+        {
+            UserName= username;
+        }
+        ShoppingCart() { }
+
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> items { get; set; } = new List<ShoppingCartItem>();
+        public decimal TotalPrice { 
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in items)
+                {
+                    totalPrice += item.Price;
+                }
+                return totalPrice; 
+            }
+                }
+    }
+}
